cb258b54ab8090603a43e4139548f5c822beb1b0
jakubbaron <jakubaron@gmail.com>
2015-04-15 15:53:30 +0100

    Jacobi fix
    Fixed jacobi's iteration, was mixed with Gauss

--- Source.cpp
+++ Source.cpp
@@ -7,16 +7,17 @@
 #include<cmath>
 
 //configuration values
-#define KMAX 10000
+#define KMAX 50000
 #define N 100
 #define MAX 1.0
 #define MIN 0.0
-#define EPSILON 10e-9
+#define EPSILON 1e-6
 
-typedef float floatType;
+typedef double floatType;
 void rewrite(floatType* from, floatType* dest, int size);
 void show(floatType** t, int size);
 void zero(floatType **t, int size);
+void zero(floatType *t, int size);
 void writeToFile(floatType **t, int size);
 
 int main(){
@@ -24,7 +25,8 @@ int main(){
 	floatType *lastRow;
 	floatType h = (MAX - MIN) / (N + 0.0);
 	floatType diff;
-	floatType temp;
+	floatType lastValue;
+	floatType leftTemp;
 	bool continueLoop = false;
 	int i, j, k;
 
@@ -39,31 +41,35 @@ int main(){
 	zero(t, N + 1);
 
 	for (i = 0; i <= N; ++i) {
-		t[i][0] = sin(M_PI * i * h) * sin(M_PI*i*h);
+		t[i][0] = sin(M_PI * i * h) * sin(M_PI * i * h);
 	}
 	std::cout << "\n\nBefore loop: \n\n";
 	//show(t, N + 1);
 
+	
 	std::cout << "\n\nloop: \n\n";
 	for (k = 0; k < KMAX; ++k) {
 		diff = 0.0;
 
+		zero(lastRow, N + 1);
 		for (i = 1; i < N; ++i) {
-			rewrite(t[i - 1], lastRow, N); //not needed +1 because we never update the last column
+			//rewrite(t[i - 1], lastRow, N); //not needed +1 because we never update the last column
+			lastRow[0] = t[i][0];
 
 			for (j = 1; j < N; ++j) {
-				temp = t[i][j];
-				t[i][j] = (t[i + 1][j] + lastRow[j] + t[i][j + 1] + t[i][j - 1]) / 4.0;
-				diff += fabs(temp - t[i][j]);
+				lastValue = t[i][j];
+				t[i][j] = (t[i + 1][j] + lastRow[j] + t[i][j + 1] + lastRow[j-1]) / 4.0;
+				lastRow[j] = lastValue;
+				diff += fabs(lastValue - t[i][j]);
 			}
 		}
-
-		if (diff - EPSILON <= 0){
+		//show(t, N);
+		if (diff < EPSILON){
 			std::cout << "Desired accuracy reached. " << diff << " iterations: " << k;
 			break;
 		}
 	}
-	std::cout << "\n\nAfter loop: \n\n";
+	std::cout << "\n\nAfter loop: \n\n" << diff;
 	//show(t, N + 1);
 
 	std::cout << "\nThe time passed: " << (clock() - startTime) / 1000.0 << "s\n";
@@ -83,7 +89,9 @@ int main(){
 }
 
 void rewrite(floatType* from, floatType* dest, int size){
+	std::cout << "\n";
 	for (int i = 0; i < size; ++i){
+		std::cout << from[i] << "\t";
 		dest[i] = from[i];
 	}
 }
@@ -92,9 +100,16 @@ void show(floatType** t, int size){
 	std::cout << "\n";
 	for (int i = 0; i < size; ++i){
 		for (int j = 0; j < size; ++j){
-			std::cout << t[i][j] << "\t";
+			//std::cout << t[i][j] << "\t";
+			printf("%.6f\t", t[i][j]);
 		}
 		std::cout << "\n";
+	}
+}
+
+void zero(floatType *t, int size){
+	for (int i = 0; i < size; ++i){
+		t[i] = 0.0;
 	}
 }
 
